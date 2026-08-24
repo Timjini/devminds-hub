503350640df9b1d40b3e55681b7adc4b05c0fd27
Timjini <hatim.jini@gmail.com>
2026-08-24 06:50:03 +0200

    devkit

---
+++ apps/brand-site/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+import {ClassicHero } from "@devminds-kit/ui";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <main>
+        <ClassicHero
+        />
+      </main>
+    </div>
+  );
+}
